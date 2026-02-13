39f3c89cb1fa6de4dffc7a66e702bad0ac5110a0
Toby Sanders <sanderst26@barker.college>
2026-02-13 13:41:59 +1100

    Added footer control, added front hero/landing page, added footer

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return(render_template("index.html"))
+    return(render_template("index.html", show_footer=True))
 
 @app.route('/login')
 def login():
-    return(render_template("login.html"))
+    return(render_template("login.html", show_footer=False))
 
 @app.route('/signup')
 def signup():
-    return(render_template("signup.html"))
+    return(render_template("signup.html", show_footer=False))
 
 if __name__ == '__main__':
     app.run(debug=True)
